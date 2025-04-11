10ade0aca59665a9e147ed3fa46f58348f798033
Audrius <apasvenskas@outlook.com>
2025-04-11 15:20:21 -0700

    added different delta frames

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import cv2
 import time
 
 video = cv2.VideoCapture(0)
+time.sleep(1)
+
+first_frame = None
 
 while True:
-    time.sleep(1)
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    
+    
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    cv2.imshow("My video", delta_frame)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('My video', thresh_frame)
 
     key = cv2.waitKey(1)
 
