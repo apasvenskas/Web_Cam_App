19f35f3bfcb510f68221cbbb32d1b8ce6847a5c1
Audrius <apasvenskas@outlook.com>
2025-04-11 19:18:23 -0700

    motion detection success

--- main.py
+++ main.py
@@ -17,9 +17,21 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
     cv2.imshow("My video", delta_frame)
-    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    cv2.imshow('My video', thresh_frame)
+    # adjust the first value currently 40, for correct black/white contrast.
+    thresh_frame = cv2.threshold(delta_frame, 40, 255, cv2.THRESH_BINARY)[1] 
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow('My video', dil_frame)
 
+    countours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # If there are two objects there must be two countours
+    for contour in countours:
+        if cv2.contourArea(contour) < 10000: # adjust the value for the amount of pixels to be detected.
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+y, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
