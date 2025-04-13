3e74240a569ca425c21172ed5b2251dca3fbb17e
Audrius <apasvenskas@outlook.com>
2025-04-13 14:28:44 -0700

    image extraction

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2
 import time
 from electronic_mail import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -35,6 +37,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+y, y+h), (0, 255, 0), 3)
         if rectangle.any(): # send email if movement detected.
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
             
     status_list.append(status) # changes the status based on if object is detected.
     status_list = status_list[-2:]
