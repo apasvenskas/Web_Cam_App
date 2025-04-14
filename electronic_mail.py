948569f776363d033ac2dea2fe0b4ab8c643ac78
Audrius <apasvenskas@outlook.com>
2025-04-14 14:25:45 -0700

    send email is configured working on issue with image path

--- electronic_mail.py
+++ electronic_mail.py
@@ -1,2 +1,30 @@
-def send_email():
-    print("Email was sent.")
+import imghdr
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+password = os.getenv("PASSWORD")
+email_addr = "audrius13toto@gmail.com"
+receiver = "audrius13toto@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage
+    email_message["Subject"] = "New object detected"
+    email_message.set_content()
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype = "image", subtype = imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gamil.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(email_addr, password)
+    gmail.sendmail(email_addr, password, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
